24c7d75cc4695f83ec481dbdb44d5f16f349bb2d
Tomasz Brylski <brylskitomasz@gmail.com>
2024-01-10 00:58:29 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -33,6 +33,15 @@ function App() {
     );
   };
 
+  const setAllDone = () => {
+    setTasks((tasks) =>
+      tasks.map((task) => ({
+        ...task,
+        done: true,
+      }))
+    );
+  };
+
   return (
     <Container>
       <Header title="Task List" />
@@ -53,6 +62,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,20 +1,21 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
-    tasks.length > 0 && (
-        <div className="buttons">
-            <button onClick={toggleHideDone} className="buttons__button">
-                {tasks.some(({ done }) => done) && hideDone ? "Show" : "Hide"} tasks done
-            </button>
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) =>
+  tasks.length > 0 && (
+    <div className="buttons">
+      <button onClick={toggleHideDone} className="buttons__button">
+        {tasks.some(({ done }) => done) && hideDone ? "Show" : "Hide"} tasks
+        done
+      </button>
 
-            <button className="buttons__button"
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Mark all tasks as done
-            </button>
-        </div>
-    )
-);
-
+      <button
+        onClick={setAllDone}
+        className="buttons__button"
+        disabled={tasks.every(({ done }) => done)}
+      >
+        Mark all tasks as done
+      </button>
+    </div>
+  );
 
 export default Buttons;
