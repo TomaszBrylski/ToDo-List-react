a22675fe3828fe28dff8ad6aba353cc2ab2b2208
Tomasz Brylski <brylskitomasz@gmail.com>
2024-01-10 10:47:30 +0100

    adding task id

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -21,7 +21,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
             task.done ? " tasks__content--done" : ""
           }`}
         >
-          {task.content}
+          {task.id} - {task.content}
         </span>
 
         <button
