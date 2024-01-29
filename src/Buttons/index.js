3e58910eb267bbd5e71647465fec7434e6fcdd73
Tomasz Brylski <brylskitomasz@gmail.com>
2024-01-29 13:32:02 +0100

    Remove className and added Wrapper and Button to import

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,21 +1,21 @@
-import "./style.css";
+import { Wrapper, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) =>
-  tasks.length > 0 && (
-    <div className="buttons">
-      <button onClick={toggleHideDone} className="buttons__button">
-        {tasks.some(({ done }) => done) && hideDone ? "Show" : "Hide"} tasks
-        done
-      </button>
-
-      <button
-        onClick={setAllDone}
-        className="buttons__button"
-        disabled={tasks.every(({ done }) => done)}
-      >
-        Mark all tasks as done
-      </button>
-    </div>
-  );
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
+  <Wrapper>
+    {tasks.length > 0 && (
+      <>
+        <Button onClick={toggleHideDone}>
+          {hideDone ? "Show" : "Hide"} tasks done
+        </Button>
+        <Button
+          onClick={setAllDone}
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Mark all tasks as done
+        </Button>
+      </>
+    )}
+  </Wrapper>
+);
 
 export default Buttons;
