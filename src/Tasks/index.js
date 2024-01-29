69cd613424e392351f712f0176bc08844040db79
Tomasz Brylski <brylskitomasz@gmail.com>
2024-01-29 13:39:19 +0100

    Formated code

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,13 +3,23 @@ import { List, Item, Content, Button } from "./styled";
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
     {tasks.map((task) => (
-      <Item key={task.id} hidden={task.done && hideDone}>
-        <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
+      <Item 
+        key={task.id} 
+        hidden={task.done && hideDone}
+      >
+        <Button 
+          toggleDone 
+          onClick={() => toggleTaskDone(task.id)}
+        >
           {task.done ? "✓" : ""}
         </Button>
-        <Content done={task.done}>{task.content}</Content>
-
-        <Button remove onClick={() => removeTask(task.id)}>
+        <Content done={task.done}>
+          {task.content}
+        </Content>
+        <Button 
+        remove 
+        onClick={() => removeTask(task.id)}
+        >
           🗑
         </Button>
       </Item>
