e223a3bb9e76fd6345f287971ddecf1b0b15b608
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-12 20:42:35 +0100

    Added border style to Input

--- src/features/tasks/Form/styled.js
+++ src/features/tasks/Form/styled.js
@@ -12,7 +12,7 @@ export const StyledForm = styled.form`
 
 export const Input = styled.input`
   padding: 10px;
-  border: ${({ theme }) => theme.color.alto};
+  border: 1px solid ${({ theme }) => theme.color.alto};
 `;
 
 export const Button = styled.button`
