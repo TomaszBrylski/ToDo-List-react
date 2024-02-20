8cd97ac36985aa4854bd3e2172b70cdf3902c51f
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-20 10:16:35 +0100

    A new structure has been added to the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import Tasks from "./features/tasks/Tasks";
+import Tasks from "./features/tasks/TasksPage";
+import Task from "./features/tasks/TaskPage";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks />;
+import { 
+  HashRouter, 
+  Switch, 
+  Route, 
+  Redirect 
+} from "react-router-dom";
+import { toAuthor, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
-export default App;
+export default () => (
+  <HashRouter>
+    <Navigation/>
+      
+      <Switch>
+        <Route path={toTask()}>
+          <Task />
+        </Route>
+        <Route path={toTasks()}>
+          <Tasks />
+        </Route>
+        <Route path={toAuthor()}>
+          <Author />
+        </Route>
+        <Route>
+          <Redirect to={toTasks()} />
+        </Route>
+      </Switch>
+  </HashRouter>
+);
+
